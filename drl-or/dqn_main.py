864fe76bee21b29df1ed4e6c844795c3657e930f
valley12 <xuxiaodong765@gmail.com>
2025-01-17 00:32:58 +0800

    add dqn main

---
+++ drl-or/dqn_main.py
@@ -0,0 +1,81 @@
+import argparse
+from net_env.my_simenv import NetEnv
+import torch
+from dqn.dqn_agent import DQNAgent
+
+def get_args():
+
+    parser = argparse.ArgumentParser(description='RL')
+
+
+    parser.add_argument('--seed', type=int, default=1,
+                        help='random seed (default: 1)')
+    parser.add_argument('--cuda-deterministic', action='store_true', default=False,
+                        help="sets flags for determinism when using CUDA (potentially slow!)")
+
+    # 智能体的模式 单智能体或者多智能体
+    # 多智能体情况下每个智能体选择下一跳的节点 组成路径 path
+    # 单智能体情况下智能体从K条候选路径中选择一条 path
+    parser.add_argument('--agent_mode', type=str, default="singe_agent",
+                        help='agent mode(single agent or multi agent)')
+    # 单智能体的动作空间
+    parser.add_argument('--k_path_num', type=int, default=5,
+                        help='k candidate path for src to route all paths')
+
+    # 是否区分不同类型的流量，当type为1时不区分流量，主要是针对强化学习的奖励函数设计
+    parser.add_argument('--service_type_num', type=int, default=1,
+                        help='service type for flows')
+    # 拓扑环境名称 Abi、GEA、GBN
+    parser.add_argument('--env_name', type=str, default="Abi",
+                        help='environment name')
+    # 流量矩阵
+    parser.add_argument("--demand-matrix", default='test.txt',
+                        help='demand matrix input file name (default:test.txt)')
+
+    parser.add_argument('--no-cuda', action='store_true', default=False,
+                        help='disables CUDA training')
+
+    # 模型加载路径
+    parser.add_argument("--model-load-path", default=None,
+                        help='load model parameters from the model-load-path')
+    # 模型保存路径
+    parser.add_argument("--model-save-path", default=None,
+                        help='save model parameters at the model-save-path')
+
+
+    args = parser.parse_args()
+    # 不使用cuda 并且 cuda 可用
+    args.cuda = not args.no_cuda and torch.cuda.is_available()
+
+    return args
+
+
+def main():
+
+    args = get_args()
+    print(f"agent_mode: {args.agent_mode} ")
+    print(f"k_path_num: {args.k_path_num} ")
+    print(f"service type num: {args.service_type_num}")
+
+    torch.manual_seed(args.seed)
+    torch.cuda.manual_seed_all(args.seed)
+    # for reproducible
+    # 检查 cuda 是否可用
+    if args.cuda and torch.cuda.is_available() and args.cuda_deterministic:
+        torch.backends.cudnn.benchmark = False
+        torch.backends.cudnn.deterministic = True
+
+    env = NetEnv(args)
+
+    observation_space, action_space = env.setup(args.env_name, args.demand_matrix)
+    print(f"agent observation_space {observation_space}, action spaces {action_space}")
+
+    # 初始化 dqn 智能体
+    state_size = observation_space.shape[0]
+    action_size = action_space.n
+    agent = DQNAgent(state_size=state_size, action_size=action_size)
+
+
+if __name__ == "__main__":
+
+    main()
